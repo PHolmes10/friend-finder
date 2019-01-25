68de8eee591044b9d10309c075071417c2c04fb0
pholmes10 <pholmes10@gmail.com>
2019-01-25 10:46:40 -0600

    set up jaws db with some dummy data

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,7 +2,7 @@ var mysql = require("mysql");
 
 var connection;
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+    connection = mysql.createConnection('mysql://o7lzlvsj6z2g6f6z:bth9zwwecm4qa3tl@s554ongw9quh1xjs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/vp82io1bbdx8hi5r');
 } else {
     var connection = mysql.createConnection({
         host: "localhost",
@@ -36,15 +36,8 @@ module.exports = function (app) {
     });
 
     app.post("/api/friends", function (req, res) {
-        // console.log('we got the route!!!', req.body)
         var user = (req.body.scores).join();
-        // console.log(scores);
         connection.query("SELECT * FROM profiles", function (err, stuffWeGotBack) {
-
-            // console.log('we got this back from DB!!', stuffWeGotBack);
-            // var data = JSON.stringify(stuffWeGotBack);
-            // data = JSON.parse(data);
-            // console.log(data)
             var match = {
                 matchName: "",
                 matchPhoto: "",
@@ -56,31 +49,16 @@ module.exports = function (app) {
             for (var i = 0; i < req.body.scores.length; i++) {
                 saveTotal += parseInt(req.body.scores)
             }
-            // console.log('this is our saveTotal', saveTotal);
 
 
             var closestDifference = 10000
             for (var k = 0; k < stuffWeGotBack.length; k++) {
-                console.log('new dude total', saveTotal)
-                console.log('compare dude totatl', stuffWeGotBack[k].totalscore);
                 var difference = saveTotal - stuffWeGotBack[k].totalscore
                 if (Math.abs(difference) < closestDifference) {
                     match = stuffWeGotBack[k]
                 }
             }
 
-            // res.json(match)
-            // var difference = 0;
-
-            // for (i = 0; i < data.length; i++) {
-            //     // console.log(data)
-            //     for (j = 0; j < data[i].scores.length; j++) {
-            //         // console.log(data[i].scores);
-            //         difference = Math.abs(data[i].scores[j] - user[j]);
-            //         // console.log(difference);
-            //     };
-
-            // }
             res.json(match)
         })
 
@@ -89,7 +67,6 @@ module.exports = function (app) {
         for (var i = 0; i < req.body.scores.length; i++) {
             saveTotal += parseInt(req.body.scores)
         }
-        console.log('this is our saveTotal down right before we save', saveTotal);
 
         connection.query("INSERT INTO profiles (name, photo, totalscore) VALUES (?, ?, ?)", [req.body.name, req.body.photo, saveTotal]);
 
